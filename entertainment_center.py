dcfd0150e66daada5da88ce0bfd4ef37b7aca40d
Brian Johnson <briancjohnson1982@gmail.com>
2017-01-25 09:31:59 +0800

    added docstrings and changed comments to docstrings

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,19 +1,24 @@
+"""This module creates a website that features information about
+some of my favorite movies.
+
+"""
+__author__ = 'Brian Johnson'
+__version__ = '0.1'
+__date__ = "Jan. 25, 2017"
+__email__ = "briancjohnson1982@gmail.com"
+
 import media
 import html_gen
 
-DEBUG = True;
+movie_titles = ["The Empire Strikes Back", "The Godfather",
+                "The Princess Bride", "Casablanca", "Dead Poets Society",
+                "Akira", "Office Space","The Matrix", "Fight Club"]
 
-movie_titles = ["The Empire Strikes Back", "The Godfather", "The Princess Bride",
-"Casablanca", "Dead Poets Society", "Akira", "Office Space","The Matrix",
-"Fight Club"]
-
-# create an array of movie objects
+# Create an array of movie objects
 movies = []
 for title in movie_titles:
     movie = media.Movie(title)
     movies.append(movie)
-    if(DEBUG):
-        movie.print_info()
 
-
+# Send array to html_gen to create the html file
 html_gen.open_movies_page(movies)
--- media.py
+++ media.py
@@ -3,7 +3,23 @@ import urllib
 import json
 import re
 
+
 class Movie():
+
+    """Create a movie object from its title.
+
+    Args:
+        title (str): the movie's title
+
+    Attributes:
+        title (str): the movie's title
+        poster_url (str): a url to and image of the movie's poster
+        trailer_youtube_id (str): the youtube id number for the movie's trailer
+        omdb_api_string (str): the title formatted for use in client-side calls
+        to the Open Movie Database. See: https://www.omdbapi.com/ for more
+        information on the API
+
+    """
 
     def __init__(self, title):
         self.title = title
@@ -11,22 +27,23 @@ class Movie():
         self.trailer_youtube_id = self.get_youtube_id(title)
         self.omdb_api_string = self.format_title(title)
 
-    # get poster url from local file
     def get_poster_url(self, title):
+        """Get poster url from local file"""
         movie_data = self.get_movie_data(title)
         poster_url = movie_data["poster"]
         return poster_url
 
-    # get youtube id from local file
     def get_youtube_id(self, title):
+        """Get youtube id from local file"""
         movie_data = self.get_movie_data(title)
         trailer_youtube_url = movie_data["youtube"]
         trailer_youtube_id = self.extract_youtube_id(trailer_youtube_url)
         return trailer_youtube_id
 
-    # moved this here from the main html generation file
-    # Extract the youtube ID from the url
+    # This was moved from the main html generation file since other information
+    # is being formatted here.
     def extract_youtube_id(self, trailer_youtube_url):
+        """Extract youtube ID from the url"""
         youtube_id_match = re.search(
             r'(?<=v=)[^&#]+', trailer_youtube_url)
         youtube_id_match = youtube_id_match or re.search(
@@ -35,24 +52,25 @@ class Movie():
                               else None)
         return trailer_youtube_id
 
-    #get information about this title in json format from local file
+    # Helper function for retrieving JSON data from a local file
     def get_movie_data(self, title):
+        """Open JSON file and return contents"""
         movie_file = open("movie_info.json")
         movie_data = json.loads(movie_file.read())
         movie_file.close()
         return movie_data[title]
 
-    #format the title for use in client-side calls to the Open Movie Data Base
-    # See: https://www.omdbapi.com/ for more information on the API
     def format_title(self, title):
+        """Format the title for calls to the Open Movie Database API."""
         formatted_title = title.lower()
         title_words = formatted_title.split()
         separator = "+"
         formatted_title = separator.join(title_words)
         return formatted_title
 
-    # printing function for testing
+    # Printing function for testing
     def print_info(self):
+        """Print the object's member variables values"""
         print("Title: " + self.title)
         print("Poster: "  + self.poster_url)
         print("Trailer ID: " + self.trailer_youtube_id)
